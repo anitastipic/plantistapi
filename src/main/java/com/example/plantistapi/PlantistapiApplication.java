0c8f3398af5059f07afb004e3d9e5107eebd5ad1
anitastipic <mailforanitastipic@gmail.com>
2024-01-25 12:28:02 +0100

    set up project

---
+++ src/main/java/com/example/plantistapi/PlantistapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.plantistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantistapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantistapiApplication.class, args);
+	}
+
+}
