7ddccfe3398e48d488c01deb9d0a654645c54c21
anitastipic <mailforanitastipic@gmail.com>
2024-01-26 22:55:39 +0100

    accomondated to changes of service classes

--- src/main/java/com/example/plantistapi/runner/PlantRunner.java
+++ src/main/java/com/example/plantistapi/runner/PlantRunner.java
@@ -1,7 +1,7 @@
 package com.example.plantistapi.runner;
 
 import com.example.plantistapi.model.Plant;
-import com.example.plantistapi.service.HousePlantsService;
+import com.example.plantistapi.service.ApiPlantService;
 import com.example.plantistapi.service.PlantService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.CommandLineRunner;
@@ -13,15 +13,15 @@ import org.springframework.context.annotation.Profile;
 @Profile("!dev")
 public class PlantRunner {
     @Bean
-    CommandLineRunner initializeDbWithPlants(HousePlantsService housePlantsService, PlantService plantService) {
+    CommandLineRunner initializeDbWithPlants(ApiPlantService apiPlantService, PlantService plantService) {
         return args -> {
             ObjectMapper mapper = new ObjectMapper();
             try {
-                String jsonPlants = housePlantsService.fetchPlants();
+                String jsonPlants = apiPlantService.fetchPlants();
 
                 PlantDto[] plantDtos = mapper.readValue(jsonPlants, PlantDto[].class);
                 for (PlantDto plantDto : plantDtos) {
-                    Plant plant = plantService.convertToPlantEntity(plantDto);
+                    Plant plant = apiPlantService.convertToPlantEntity(plantDto);
                     plantService.save(plant);
                 }
                 System.out.println("Plants saved!");
